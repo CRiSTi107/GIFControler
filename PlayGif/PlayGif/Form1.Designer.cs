64663dbfc35478e77c7450128653df49308c628e
Voiculescu Cristian Constantin <31419972+CRiSTi107@users.noreply.github.com>
2017-10-08 15:00:34 +0300

    Add files via upload

---
+++ PlayGif/PlayGif/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PlayGif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_Gif = new System.Windows.Forms.PictureBox();
+            this.cmb_Index = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Gif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_Gif
+            // 
+            this.pic_Gif.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pic_Gif.Location = new System.Drawing.Point(12, 12);
+            this.pic_Gif.Name = "pic_Gif";
+            this.pic_Gif.Size = new System.Drawing.Size(460, 411);
+            this.pic_Gif.TabIndex = 0;
+            this.pic_Gif.TabStop = false;
+            // 
+            // cmb_Index
+            // 
+            this.cmb_Index.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cmb_Index.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_Index.FormattingEnabled = true;
+            this.cmb_Index.Location = new System.Drawing.Point(185, 429);
+            this.cmb_Index.Name = "cmb_Index";
+            this.cmb_Index.Size = new System.Drawing.Size(121, 21);
+            this.cmb_Index.TabIndex = 2;
+            this.cmb_Index.SelectedIndexChanged += new System.EventHandler(this.cmb_Index_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.cmb_Index);
+            this.Controls.Add(this.pic_Gif);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Playing Gif";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Gif)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_Gif;
+        private System.Windows.Forms.ComboBox cmb_Index;
+    }
+}
+
---
+++ PlayGif/PlayGif/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayGif
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        Image gifImage;
+        FrameDimension dimension;
+        int frameCount;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //pic_Gif.Image = Image.FromFile("bg.gif"); = 0 work 4 me
+
+            const string gifPath = "ekko.gif";
+
+            gifImage = Image.FromFile(gifPath);
+            dimension = new FrameDimension(gifImage.FrameDimensionsList[0]);
+            frameCount = gifImage.GetFrameCount(dimension);
+
+            for (int i = 0; i <= frameCount - 1; i++)
+                cmb_Index.Items.Add(i);
+
+            base.Size = new Size(gifImage.Size.Width + 40, gifImage.Size.Height + 89);
+            base.Text = "GifName='" + gifPath + "' | FrameCount=" + frameCount;
+            //base.Text = "GifPath='" + System.IO.Path.GetFullPath(gifPath) + "' | FrameCount=" + frameCount; 
+        }
+
+        private void cmb_Index_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedFrame = Convert.ToInt32(cmb_Index.SelectedItem);
+
+            gifImage.SelectActiveFrame(dimension, selectedFrame);
+            pic_Gif.BackgroundImage = new Bitmap((Image)gifImage.Clone());
+        }
+    }
+}
